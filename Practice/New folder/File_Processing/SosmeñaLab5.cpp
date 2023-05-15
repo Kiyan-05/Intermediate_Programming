95bfecf051a653875d22f4ed37aaba1f74698b33
Kiyan-05 <joshsosme@gmail.com>
2023-05-15 15:10:36 +0800

    file processing code

---
+++ Practice/New folder/File_Processing/SosmeñaLab5.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+using namespace std;
+
+struct student
+{
+    string studentName;
+    double score;
+};
+
+void readFromRecord(student studScoreRec[]);
+double recAverage(student studScoreRec[], int s);
+double recSTDev(student studScoreRec[], int s);
+void writeResultToFile(double ave, double stDev);
+
+
+int main()
+{
+    double ave, stDev;
+    int s = 7;
+    student studScoreRec[s];
+
+    readFromRecord(studScoreRec);
+    ave = recAverage(studScoreRec,s);
+    stDev = recSTDev(studScoreRec,s);
+
+    writeResultToFile(ave,stDev);
+}
+
+void readFromRecord(student studScoreRec[])
+{
+    ifstream inStudentScore;
+
+    inStudentScore.open("StudentScore.txt");
+
+//    if(!inStudentScore.eof())
+//    {
+//        for(int i=0; i<7; i++)
+//        {
+//            getline(inStudentScore,studScoreRec[i].studentName, ' ');
+//            inStudentScore>>studScoreRec[i].score;
+//        }
+//    }
+
+    for(int i=0; !inStudentScore.eof(); i++)
+    {
+        getline(inStudentScore,studScoreRec[i].studentName, ' ');
+        inStudentScore>>studScoreRec[i].score;
+    }
+}
+
+double recAverage(student studScoreRec[], int s)
+{
+    double res = 0;
+
+    for(int i=0; i<s; i++)
+    {
+        res += studScoreRec[i].score;
+    }
+
+    return res/s;
+}
+
+double recSTDev(student studScoreRec[], int s)
+{
+    double mean = recAverage(studScoreRec,s);
+    double x = 0, fn = 0, res = 0;
+
+    for(int i=0; i<s; i++)
+    {
+        x += pow(studScoreRec[i].score - mean,2);
+    }
+    fn = x/(s-1);
+    res = sqrt(fn);
+    return res;
+}
+
+void writeResultToFile(double ave, double stDev)
+{
+    ofstream outStudentScore;
+
+    outStudentScore.open("scoreDescStat.txt");
+    if(outStudentScore.is_open())
+    {
+        outStudentScore<<"Average Score: "<<ave<<"\n";
+        outStudentScore<<"Standard Deviation: "<<stDev;
+    }
+}
+
+
+
